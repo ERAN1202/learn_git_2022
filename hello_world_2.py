94b911e1cd897d5069cb074513dd20332be0970b
Eran Bar Haim <eranbarhaim@yahoo.com>
2022-01-09 21:33:19 +0200

    change_3

--- hello_world_2.py
+++ hello_world_2.py
@@ -1,3 +1,8 @@
 print("hello world II")
 print("whats up world II?")
 print("what the heck?")
+my_files=["eran.mp3", "yuval.txt" ,"avigal.txt" ,"ilanit.mp3"]
+def is_mp3_file(filename):
+    return filename.endswith(".mp3")
+
+print(list(filter(is_mp3_file, my_files)))
