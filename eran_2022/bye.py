aefb18457bf95366975d43cc7b7dff20982dcc0b
Eran Bar Haim <eranbarhaim@yahoo.com>
2022-01-11 06:47:47 +0200

    new_file_4

---
+++ eran_2022/bye.py
@@ -0,0 +1,3 @@
+"""cli collab"""
+
+print("use cli git to commit and push")
--- eran_2022/chuk_nor_ran_joke.py
+++ eran_2022/chuk_nor_ran_joke.py
@@ -3,3 +3,5 @@ import requests
 response = requests.get("https://api.chucknorris.io/jokes/random")
 joke = response.json()['value']
 print(joke)
+
+
